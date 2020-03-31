96a6e62be15537fdcc4c8c69effd489aeea1eda7
Xp3DX <Xp3DX@github.com>
2020-03-31 17:34:27 +0700

    [1]Add tags in object 'Note'. [2]Update all function for tags. [3]Add tag's search.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const listWindow = document.querySelector('.listWindow');//Window for note's lis
 //Editor's elements
 const iTitle = document.querySelector('#iTitle');//Textare for note's title
 const iText = document.querySelector('#iText');//Textare for note's text
+const iTags = document.querySelector('#iTags');
 //Instrument's elements
 const btSave = document.querySelector('.SAVE'); //Button 'Save'
 const btAdd = document.querySelector('.ADD');//Button 'Add'
@@ -15,18 +16,21 @@ var viewNT; //ID Last Note
 var baseNote = []; //Data Base for Notes
 
 //Object Notes
-function Note(title, text) {
+function Note(title, text, tags) {
     this.title = title;//Title note
     this.text = text;//Text note
+    this.tags = tags;//List tags
 }
 
 //Save current note
 function saveNote() {
     var text = iText.value;
     var title = iTitle.value;
+    var tags = iTags.value;
     var listNote = listWindow.querySelector('._' + viewNT);
     baseNote[viewNT - 1]['title'] = title;
     baseNote[viewNT - 1]['text'] = text;
+    baseNote[viewNT - 1]['tags'] = tags;
     listNote.textContent = title;
 
 
@@ -35,6 +39,7 @@ function saveNote() {
 function delNote() {
     iText.value = '';
     iTitle.value = '';
+    iTags.value = '';
     listWindow.removeChild(listWindow.querySelector('._' + viewNT));
     delete baseNote[viewNT - 1];
     viewNT = 0;
@@ -42,8 +47,11 @@ function delNote() {
 //Open note in listWindow
 function openNote(event) {
     var num = parseInt(event.target.className.slice(1, event.target.className.length)) - 1;
+
     iTitle.value = baseNote[num]["title"];
     iText.value = baseNote[num]["text"];
+    iTags.value = baseNote[num]['tags'];
+
     viewNT = num + 1;
 }
 
@@ -51,16 +59,24 @@ function openNote(event) {
 function searchNote(request) {
     var reg = new RegExp(request, 'i');
     var lists = listWindow.childNodes;
-    var lister = "HJee";
-    lister.search(request)
-    for (let i = 0; i < lists.length; i++) {
-        if (baseNote[i]['title'].search(reg) === -1) {
-            lists[i].style.display = 'none';
-            continue;
-        } else {
-            lists[i].style.display = 'block';
+    if (!(request.search(/^#/))) {
+        for (let i = 0; i < lists.length; i++) {
+            if (baseNote[i]['tags'].search(reg) !== -1) {
+                lists[i].style.display = 'none';
+                continue;
+            } else {
+                lists[i].style.display = 'block';
+            }
         }
-
+    } else {
+        for (let i = 0; i < lists.length; i++) {
+            if (baseNote[i]['title'].search(reg) === -1) {
+                lists[i].style.display = 'none';
+                continue;
+            } else {
+                lists[i].style.display = 'block';
+            }
+        }
     }
 
 
@@ -69,9 +85,12 @@ function searchNote(request) {
 //Add new note
 function addNote() {
     var newNote = document.createElement("li");
+
     var title = iTitle.value;
     var text = iText.value;
-    var note = new Note(title, text);
+    var tags = iTags.value;
+
+    var note = new Note(title, text, tags);
     newNote.textContent = title;
     newNote.className = '_' + baseNote.push(note);
     newNote.onclick = function () { openNote(event); };
